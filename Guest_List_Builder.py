19f3c65a2be84b5f5029100ef2b30c59d200450b
Ramon Simbulan <ebm569@mail.usask.ca>
2025-03-15 00:29:27 -0600

    Add single rating question with input validation

--- Guest_List_Builder.py
+++ Guest_List_Builder.py
@@ -1,16 +1,34 @@
 class Guest:
-  def __init__(self):
+  def __init__(self, name):
     self.name = name
     self.total_score = 0
+
+def get_rating(question):
+  while True:
+    try:
+      rating = int(input(question + " (1-5): "))
+      if 1 <= rating <= 5:
+        return rating
+      else:
+        print("Please enter a number between 1 to 5")
+    except ValueError:
+      print("Enter a valid number")
+
+def rate_guest(guest):
+  print(f"{guest.name}:")
+  guest.total_score = get_rating("How close are you to this person? (1 = acquaintance, 5 = Family/Bffs)")
 
 def main():
   print("Wedding Guest List Builder")
   guests = []
 
   while True:
-    name = input("Enter guest name (TYPE: 'done' to finish): ")
+    name = input("Enter guest name (TYPE: 'done' to finish): ").strip()
+
     if name.lower() == 'done':
       break
+    guest = Guest(name)
+    rate_guest(guest)
     guests.append(name)
 
   # Show the list
